7fac7f482185ad53bca9541bfd799f23421dfa67
zodiuxus <22411434+zodiuxus@users.noreply.github.com>
2025-04-10 12:34:56 +0200

    Update ackermann_control.py

--- ackermann_control/ackermann_control/ackermann_control.py
+++ ackermann_control/ackermann_control/ackermann_control.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from ackermann_msgs.msg import AckermannDrive
+from ackermann_msgs.msg import AckermannDriveStamped, AckermannDrive
 import math
 import sys
 import os
@@ -30,74 +30,101 @@ message = """
 Ackermann Control Node
 ---------------------------
 Moving around:
-          ^
-          w 
-   < a    s    d >
+             ^
+            w/i 
+   < a/j    s/k    d/l >
 ---------------------------
-w : increase speed
-s : decrease speed
-a : turn left
-d : turn right
-r : steering angle to 0
-q : to stop and quit
+w/i : increase speed
+s/k : decrease speed
+a/j : turn left
+d/l : turn right
+r/o : steering angle to 0
+q   : to stop and quit
 """
 
 class ackermannControl(Node):
     def __init__(self):
         super().__init__('Ackermann_Control')
-        self.publisher_ = self.create_publisher(AckermannDrive, '/cmd_ackermann', 10) 
+        self.publisher_ego_ = self.create_publisher(AckermannDriveStamped, '/drive', 10) 
+        self.publisher_opp_ = self.create_publisher(AckermannDriveStamped, '/opp_drive', 10) 
         timer_period = 0.1
         self.timer = self.create_timer(timer_period, self.timer_callback)
         self.speed = 0.0
         self.steering_angle = 0.0
         self.lines = []
-        print(message)
+        self.opp_speed = 0.0
+        self.opp_steering_angle = 0.0
 
 
     def timer_callback(self):
-        msg = AckermannDrive()
-        msg.steering_angle_velocity = math.pi/72
-        msg.acceleration = 0.5
+        msg = AckermannDriveStamped()
+        msg_opp = AckermannDriveStamped()
+        msg_opp.header.stamp = self.get_clock().now().to_msg()
+        msg_opp.drive.steering_angle_velocity = math.pi/72
+        msg_opp.drive.acceleration = 0.5
+        msg.header.stamp = self.get_clock().now().to_msg()
+        msg.drive.steering_angle_velocity = math.pi/72
+        msg.drive.acceleration = 0.5
         key = getKey(settings=termios.tcgetattr(sys.stdin.fileno()))
         
         if key.lower() == 'w':
             self.speed = self.speed + 1.0
         elif key.lower() == 'a':
-            self.steering_angle = self.steering_angle - math.pi/36
+            self.steering_angle = self.steering_angle + math.pi/36
         elif key.lower() == 's':
             self.speed = self.speed - 1.0
         elif key.lower() == 'd':
-            self.steering_angle = self.steering_angle + math.pi/36
+            self.steering_angle = self.steering_angle - math.pi/36
         elif key.lower() == 'r':
             self.steering_angle = 0.0
+
+        elif key.lower() == 'i':
+            self.opp_speed = self.opp_speed + 1.0
+        elif key.lower() == 'j':
+            self.opp_steering_angle = self.opp_steering_angle + math.pi/36
+        elif key.lower() == 'k':
+            self.opp_speed = self.opp_speed - 1.0
+        elif key.lower() == 'l':
+            self.opp_steering_angle = self.opp_steering_angle - math.pi/36
+        elif key.lower() == 'o':
+            self.opp_steering_angle = 0.0
+
         elif key.lower() == 'q':
             self.speed = 0.0
             self.steering_angle = 0.0
-            msg.steering_angle = self.steering_angle
-            msg.speed = self.speed
-            self.publisher_.publish(msg)
+            self.opp_speed = 0.0
+            self.opp_steering_angle = 0.0
+            msg_opp.drive.steering_angle = self.opp_steering_angle
+            msg.drive.steering_angle = self.steering_angle
+            msg_opp.drive.speed = self.opp_speed
+            msg.drive.speed = self.speed
+            self.publisher_ego_.publish(msg)
+            self.publisher_opp_.publish(msg)
             messages = "speed: " + str(self.speed) + " steering_angle: " + str(self.steering_angle)
             print(messages)
             sys.exit(0)
         messages = "speed: " + str(self.speed) + " steering_angle: " + str(self.steering_angle)
         print(messages)
         self.lines.append(messages)
-        msg.steering_angle = self.steering_angle
-        msg.speed = self.speed
-        self.publisher_.publish(msg)
         
+        msg_opp.drive.steering_angle = self.opp_steering_angle
+        msg.drive.steering_angle = self.steering_angle
+        msg_opp.drive.speed = self.opp_speed
+        msg.drive.speed = self.speed
+        self.publisher_ego_.publish(msg)
+        self.publisher_opp_.publish(msg_opp)
+
         if len(self.lines) > 8:
             self.lines.clear()
             clear_screen()
-            print(message)
             print(messages)
 
 
 def main(args=None):
     rclpy.init(args=args)
-    ackermann_control = ackermannControl()
-    rclpy.spin(ackermann_control)
-    ackermann_control.destroy_node()
+    drive = ackermannControl()
+    rclpy.spin(drive)
+    drive.destroy_node()
     rclpy.shutdown()
 
 if __name__ == '__main__':
